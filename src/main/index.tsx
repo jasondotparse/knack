e724dd982cf470f9c1bcff0b0574dc8aa7fb80a3
jason <jholtkamp@rallyhealth.com>
2020-05-24 18:54:36 -0500

    Added several 'new thought' screens

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,12 +3,18 @@ import { createStackNavigator } from 'react-navigation-stack';
 import ThoughtScreen from './ThoughtScreen';
 import AutomaticThoughtScreen from './AutomaticThoughtScreen';
 import DistortionScreen from './DistortionScreen';
+import ChallengeScreen from './ChallengeScreen';
+import AlternativeScreen from './AlternativeScreen';
+import FeelingScreen from './FeelingScreen';
 
 export default createStackNavigator(
   {
     'THOUGHT_SCREEN': ThoughtScreen,
     'AUTOMATIC_THOUGHT_SCREEN': AutomaticThoughtScreen,
-    'DISTORTION_SCREEN': DistortionScreen
+    'DISTORTION_SCREEN': DistortionScreen,
+    'CHALLENGE_SCREEN': ChallengeScreen,
+    'ALTERNATIVE_SCREEN': AlternativeScreen,
+    'FEELING_SCREEN': FeelingScreen
   },
   {
     initialRouteName: 'THOUGHT_SCREEN',
